73657f3163397cf5db6fdd25b9b5e50c4be91160
marcinprusko <marcinprusko@gmail.com>
2017-02-23 22:51:16 +0100

    dodane ropozytorium

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// plik scripts.js
+
+var name = prompt('Wpisz swoje imiê');
+alert('Witaj, ' + name);
+console.log('Witaj, ' + name);
+
+// komentarz kodillowy
+
+/*
+komentarz kodillowy
+komentarz kodillowy
+*/
+
+// komentarz kodillowy
